87f22eff21e5a7accd298599a07382e5a544dddc
Inna Yasynchuk <i_bybyk@ukr.net>
2024-01-07 14:22:32 +0200

    add swiper

---
+++ css/script.dev.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: -7,
+  loop: true,
+  speed: 2500,
+  grabCursor: true,
+  slideToClickedSlide: true,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: "true",
+    dynamicBullets: "true"
+  },
+  autoplay: {
+    delay: 3500
+  },
+  coverflowEffect: {}
+});
